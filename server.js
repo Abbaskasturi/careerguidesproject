c6390226dcb3f2c17c726e8b61b0525ec05a3887
kasturiabbas <kasturiabbaspatel@gmail.com>
2025-09-28 10:35:35 +0530

    enable cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const dotEnv = require('dotenv');
 const { MongoClient } = require("mongodb");
+const cors = require("cors"); 
 
 dotEnv.config();
-
+app.use(cors()); 
 const app = express();
 app.use(express.json());
 
